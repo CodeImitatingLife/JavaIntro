ee06e1cda39e67bdd601082396536bb11f6096cf
mst-csfahie <mst-csfahie@optimum.net>
2019-02-06 11:25:27 -0500

    Update Homework.java
    Updated int variable.

--- Homework.java
+++ Homework.java
@@ -1,1 +1,12 @@
+/** Content was changed as expected. **/
 
+class Tentative {
+
+  public static void main(String[] args) {
+  
+    int x = 10;
+    System.out.println(10);
+    
+  }// end of main.
+    
+}// end of class.
